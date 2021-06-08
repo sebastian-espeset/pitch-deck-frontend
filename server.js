f5f67b4fbbb89e268aa39d280c020ba05acf1144
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-06-08 14:24:22 -0500

    merging

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public");
+    cb(null, 'uploads');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
