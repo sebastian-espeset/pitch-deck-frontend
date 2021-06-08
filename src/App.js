4dcb71754445a8514d58efdbd96282d79c6f3969
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-06-07 19:46:01 -0500

    builds out axios post requests, builds out two new states, builds out changeHandler and submissionHandler

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-
+const localURL = `http://localhost:5000/`
 function App() {
-  const [message,setMessage]=useState(`Nothing`)
+  const [message, setMessage] = useState(`Nothing`);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/`)
+      .get(localURL)
       .then((res) => {
-        setMessage(res.data.message)
-        console.log(res)
+        setMessage(res.data.message);
+        console.log(res);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const changeHandler = (e) => {
+    setSelectedFile(e.target.files[0]);
+    setSelectedFile(true);
+  };
+  const handleSubmission = (e) => {
+    const formData = new FormData();
+    formData.append('File', selectedFile);
+    axios.post(`${localURL}/api/pitches`)
+  };
   return (
     <div className="App">
       <header className="App-header">
-        {message ? <h1>{message}</h1>:`nothing`}
+        {message ? <h1>{message}</h1> : `nothing`}
+        <input type="file" name="file" onChange={changeHandler} />
+        {isFilePicked ? (
+          <div>
+            <p>Filename:{selectedFile.name}</p>
+          </div>
+        ) : (
+          <p>select a file to show details</p>
+        )}
+        <button>Submit pitch</button>
       </header>
     </div>
   );
