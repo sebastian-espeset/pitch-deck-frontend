9044c926131116610f33a6f51f5573fab3db0cfc
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-06-11 13:21:24 -0500

    app works!

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import axios from "axios";
 import { Document, Page, pdfjs } from "react-pdf";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 import "./App.css";
-import testPdf from "./test.pdf";
 
-
-
-const localURL = `http://localhost:5000`
+const localURL = `http://localhost:5000`;
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
@@ -15,48 +12,47 @@ function App() {
   const [message, setMessage] = useState(`Nothing`);
   const [selectedFile, setSelectedFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
+  const [pdfDisplay, setPdfDisplay] = useState();
+  const [pdfPath, setPdfPath] = useState();
+  let [pdfPage,setPdfPage]=useState(1)
 
-  useEffect(() => {
-    axios
-      .get(`${localURL}/api/pitches`)
-      .then((res) => {
-        setMessage(res.data.message);
-        console.log(res.data)
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
   const changeHandler = (e) => {
     setSelectedFile(e.target.files[0]);
-    console.log(selectedFile)
     setIsFilePicked(true);
   };
-  
+
   const handleSubmission = (e) => {
     const data = new FormData();
-    
-    
-    data.append('file',selectedFile);
-
+    data.append("file", selectedFile);
     console.log(data);
-
-    axios.post(`${localURL}/api/pitches`,data)
-      .then(res=>{
-        console.log(res.data);
+    axios
+      .post(`${localURL}/api/pitches`, data)
+      .then((res) => {
+        setPdfPath(res.data.file);
+        console.log("pdf path", pdfPath);
       })
-      .catch(err=>{
-        console.log(err)
+      .catch((err) => {
+        console.log(err);
       });
-    setMessage("File upload complete")
-    setIsFilePicked(false)
-    setSelectedFile()
+    setMessage("File upload complete");
+    setIsFilePicked(false);
+    setSelectedFile();
   };
-
+  
+  const incPage=(e)=>{
+    setPdfPage(pdfPage++)
+    console.log(pdfPage)
+  }
+  const decPage=(e)=>{
+   setPdfPage(pdfPage--)
+   console.log(pdfPage)
+  }
   return (
     <div className="App">
       <header className="App-header">
         {message ? <h1>{message}</h1> : `nothing`}
-        <input type="file" name="file" onChange={changeHandler} formEncType="multipart/form-data"/>
+        <input type="file" name="file" onChange={changeHandler} />
         {isFilePicked ? (
           <div>
             <p>File name:{selectedFile.name}</p>
@@ -66,11 +62,18 @@ function App() {
           <p>select a file to show details</p>
         )}
         <button onClick={handleSubmission}>Submit pitch</button>
-      <div>
-          <Document file = {testPdf}>
-            <Page pageNumber={1}/>
-            </Document>
-      </div>
+        <div>
+          {pdfPath?<Document
+            file={{
+              url: `${localURL}/api/pitches${pdfPath}`,
+            }}
+            loading="loading pdf"
+          >
+            <Page pageNumber={pdfPage} />
+            <button onClick={incPage}>Page ++</button>
+            <button onClick={decPage}>Page --</button>
+          </Document>:<div>Please upload a Document</div>}
+        </div>
       </header>
     </div>
   );
